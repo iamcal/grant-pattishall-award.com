4211b33853c31635f8816ea8b12c28cf568d3316
Cal Henderson <cal@iamcal.com>
2013-02-04 15:47:58 -0800

    initial import

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>The Grant-Pattishall Award</title>
+<style type="text/css">
+body { margin-top: 68px; text-align: center; margin-bottom: 100px; }
+h1 {  margin: 0px 0 40px 0px; font: 42px Georgia; color: #40262a; }
+p { margin: 15px 0 15px 0; font: 22px Georgia; color: #40262a; }
+p.prev { margin-top: 40px; font-size:20px; line-height: 28px;}
+span.u { text-decoration: underline; font-weight: bold; }
+a {color:#027499; }
+</style>
+</head>
+<body>
+
+
+<h1>The Grant-Pattishall Award</h1>
+
+<img src="4075887842_a4597a01e3.jpg" width="500" height="375" />
+
+<p>The Grant-Pattishall Award is given yearly to the individual<br />who breaks flickr.com in the most spectacular way.</p>
+
+<p class="prev">
+	<span class="u">2013 Nominees</span><br />
+	&hellip;<br />
+	<br />
+
+	<span class="u">Previous Winners</span><br />
+	2012: Gil Raphaelli<br />
+	2011: Ben Freeman<br />
+	2010: Daniel Bogan<br />
+	2009: Kellan Elliott-McCrea<br />
+	2008: Myles Grant<br />
+	2007: Dathan Pattishall<br />
+	2006: Myles Uyema<br />
+	2005: Cal Henderson<br />
+	2004: Cal Henderson<br />
+	2003: Cal Henderson<br />
+</p>
+
+
+
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-106458-9");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+
+</body>
+</html>
+
